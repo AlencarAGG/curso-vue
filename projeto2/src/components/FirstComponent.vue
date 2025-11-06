c14098dd663af28f25141321404825f2790cee3a
Alencar <re4percorp@gmail.com>
2025-11-06 10:49:43 -0300

    AULA 15

--- projeto2/src/components/FirstComponent.vue
+++ projeto2/src/components/FirstComponent.vue
@@ -3,11 +3,17 @@
         <h1>Olá Vue</h1>
         <P>Meu Nome é {{ nome }} e trabalho como {{ profissao }}</P>
     </div>
+    <MultiplosEventos/>
 </template>
 
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue";
+
 export default {
     name: "FirstComponent",
+    components: {
+        MultiplosEventos,
+    },
     data() {
         return {
             nome: "Matheus",
